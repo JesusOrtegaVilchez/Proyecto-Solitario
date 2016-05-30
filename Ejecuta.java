53c8aae4872c5cb8596c054d527cab9b2e11517f
JesusOrtegaVilchez <jesus_a_t_g@hotmail.com>
2016-05-30 20:01:03 +0200

    Update Ejecuta.java

--- Ejecuta.java
+++ Ejecuta.java
@@ -1,20 +1,25 @@
+
 package clases;
 
 import java.awt.*;
 import javax.swing.*;
 import java.awt.event.*;
-public class Ejecuta extends JFrame{
-	/**
-	 * 
-	 */
+import java.awt.Toolkit;
+public class Ejecuta extends JFrame
+{
+	
+	
+	
 	private static final long serialVersionUID = 1L;
 	JMenuBar menu=  new JMenuBar();
 	PanelSolitario panel = new PanelSolitario();
 	JMenuItem men= new JMenuItem("Partida Nueva");
 	JMenuItem men3= new JMenuItem("Acerca del Juego");
 	JMenuItem men2= new JMenuItem("Salir");
+	
     public Ejecuta() 
     {
+    	Toolkit.getDefaultToolkit().getImage(ClassLoader.getSystemResource("dorsal_spider2.png"));
     	setLayout(new BorderLayout());
     	add(panel,BorderLayout.CENTER);
     	this.setJMenuBar(menu);
@@ -43,10 +48,12 @@ public class Ejecuta extends JFrame{
     	menu1.add(men3);
     	menu1.add(men2);
     	menu.add(menu1);
-    	panel.setBackground(Color.yellow);
-    	setSize(800,540);
+    	panel.setBackground(Color.green);
+    	setSize(1070,600);
     	setLocationRelativeTo(null);
     	setVisible(true);
+    	this.setTitle("Solitario Spider");
+    	
     }
     
     public static void main (String[] args) {
